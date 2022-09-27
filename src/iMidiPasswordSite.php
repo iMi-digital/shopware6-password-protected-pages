4ef65c9fa74346d2ee4fa84c0f9eaecbfaf37fee
Maurizio Weis <m.weis@imi.de>
2022-09-27 17:01:44 +0200

    #74040 remove custom fields on uninstall

--- src/iMidiPasswordSite.php
+++ src/iMidiPasswordSite.php
@@ -5,21 +5,37 @@ namespace iMidiPasswordSite;
 use Enqueue\Util\UUID;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsAnyFilter;
 use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\InstallContext;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\IdSearchResult;
 
 class iMidiPasswordSite extends Plugin
 {
-    public function install(InstallContext $context): void {
-        $this->createCustomFields($context);
+
+    private const CUSTOM_FIELD_NAME = 'password_site';
+
+    public function install(InstallContext $installContext): void
+    {
+        $this->createCustomFields($installContext);
+        parent::install($installContext);
     }
 
-    private function createCustomFields(InstallContext $context)
+    public function uninstall(UninstallContext $uninstallContext):void
+    {
+        if(!$uninstallContext->keepUserData()) {
+            $this->removeCustomFields($uninstallContext);
+        }
+        parent::uninstall($uninstallContext);
+    }
+
+    private function createCustomFields(InstallContext $installContext)
     {
         $customFields = [
             [
-                'name' => 'password_site',
+                'name' => self::CUSTOM_FIELD_NAME,
                 'active' => true,
                 'global' => true,
                 'config' => [
@@ -47,17 +63,35 @@ class iMidiPasswordSite extends Plugin
                 ]
             ]
         ];
-        $repo = $this->container->get('custom_field_set.repository');
 
-        $criteria = new Criteria();
-        $criteria->addFilter(new EqualsFilter('name', 'password_site'));
+        $customFieldSetRepository = $this->container->get('custom_field_set.repository');
 
-        $result = $repo->search($criteria, $context->getContext());
-
-        if($result->count() <= 0) {
+        if (!$this->customFieldsExist($customFieldSetRepository, $installContext->getContext())) {
             foreach ($customFields as $customFieldSet) {
-                $repo->upsert([$customFieldSet], $context->getContext());
+                $customFieldSetRepository->upsert([$customFieldSet], $installContext->getContext());
             }
         }
     }
+
+    private function removeCustomFields(UninstallContext $uninstallContext): void
+    {
+        $customFieldSetRepository = $this->container->get('custom_field_set.repository');
+
+        $fieldIds = $this->customFieldsExist($customFieldSetRepository, $uninstallContext->getContext());
+
+        if ($fieldIds) {
+            $customFieldSetRepository->delete(array_values($fieldIds->getData()), $uninstallContext->getContext());
+        }
+    }
+
+    private function customFieldsExist(EntityRepository $customFieldSetRepository, Context $context): ?IdSearchResult
+    {
+        $criteria = new Criteria();
+        $criteria->addFilter(new EqualsAnyFilter('name', [self::CUSTOM_FIELD_NAME]));
+
+        $ids = $customFieldSetRepository->searchIds($criteria, $context);
+
+        return $ids->getTotal() > 0 ? $ids : null;
+    }
+
 }
