124f342cefb35f121ab5a4d4e6ccd95629e0a927
Maurizio Weis <m.weis@imi.de>
2024-02-29 10:27:35 +0100

    #18 fix PasswordPageController to comply with SW Linter

--- src/Storefront/Controller/PasswordPageController.php
+++ src/Storefront/Controller/PasswordPageController.php
@@ -34,7 +34,7 @@ class PasswordPageController extends StorefrontController
         $page = $this->genericPageLoader->load($request, $salesChannelContext);
 
         return $this->renderStorefront('@ImiDiPasswordProtectedPages/storefront/page/restricted.html.twig', [
-            'navigationId' => $request->get('navigationId'),
+            'navigationId' => json_decode($request->query->get('redirectParameters'))->navigationId,
             'page' => $page,
         ]);
     }
@@ -44,7 +44,7 @@ class PasswordPageController extends StorefrontController
      */
     public function login(Request $request): Response
     {
-        $navigationId = $request->get('navigationId');
+        $navigationId = json_decode($request->query->get('redirectParameters'))->navigationId;
 
         if(!$request->request->has('password')) {
             $this->addFlash(self::DANGER, $this->trans('ImiDi.password-incorrect'));
